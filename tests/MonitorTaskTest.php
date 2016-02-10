112a45ce2c63c9206db5875a965a505de106887b
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-02-09 16:13:04 -0800

    added monitor task test case

---
+++ tests/MonitorTaskTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Cronitor\Tests;
+
+use Cronitor\Client;
+use anlutro\cURL;
+
+class ClientTest extends TestBase
+{
+
+    public function test_it_should_run()
+    {
+        $client = $this->getOkClient();
+
+        $response = $client->run();
+        $this->assertEquals($response->statusCode, 200);
+    }
+
+}
