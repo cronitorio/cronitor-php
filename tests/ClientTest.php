899938493a9433cc1032a1fce45c53775a54eff7
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-02-09 16:04:11 -0800

    added test base class

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,22 +5,8 @@ namespace Cronitor\Tests;
 use Cronitor\Client;
 use anlutro\cURL;
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+class ClientTest extends TestBase
 {
-
-    public $client;
-    protected $okResponse;
-
-    public function setUp()
-    {
-        $okHeaders = 'HTTP/1.1 200 OK
-Server: nginx/1.4.6 (Ubuntu)
-Date: Mon, 08 Feb 2016 22:42:43 GMT
-Content-Length: 0
-Connection: close';
-
-        $this->okResponse = new cURL\Response('', $okHeaders);
-    }
 
     public function test_it_should_run()
     {
@@ -70,33 +56,4 @@ Connection: close';
         $client = new Client('boogers');
 		$this->assertEquals(new cUrl\cUrl, $client->getcUrl());
 	}
-
-    protected function getOkClient()
-    {
-        $okCurl = $this->getOkCurl();
-
-        $client = $this->getMockBuilder('\Cronitor\Client')
-            ->setConstructorArgs(array('boogers'))
-            ->setMethods(array('getcUrl'))
-            ->getMock();
-
-        $client->expects($this->once())
-            ->method('getcUrl')
-            ->will($this->returnValue($okCurl));
-
-        return $client;
-    }
-
-    protected function getOkcUrl()
-    {
-        $curl = $this->getMockBuilder('\anlutro\cURL\cURL')
-            ->setMethods(array('sendRequest'))
-            ->getMock();
-
-        $curl->expects($this->once())
-            ->method('sendRequest')
-            ->will($this->returnValue($this->okResponse));
-
-        return $curl;
-    }
 }
